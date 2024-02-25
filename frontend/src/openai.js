f537bc3d07532e97932d34794591fe1047c53072
Devyansh . <devyanshnegi@gmail.com>
2024-02-25 00:36:53 -0700

    Works

--- frontend/src/openai.js
+++ frontend/src/openai.js
@@ -1,11 +1,17 @@
-import openai from 'openai';
+import OpenAI from "openai";
 
-openai.apiKey = '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';
+const openai = new OpenAI({apiKey:"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", dangerouslyAllowBrowser: true});
 
 export async function sendMsgToOpenAI(message) {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
-  console.log(completion?.choices[0]);
+  try{
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(completion.choices[0]);
+  }
+  catch(e){
+    console.log(e);
+  }
 }
